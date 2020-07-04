04c1f4995a6f31913d7ea9d3d35996aee6c804d4
Shashwath Mohan <55274230+shashwathmohan@users.noreply.github.com>
2020-07-04 05:32:53 +0530

    newfile

---
+++ bday_details/script.js
@@ -0,0 +1,32 @@
+
+
+var userDay = document.getElementById("user-day");
+var userMonth = document.getElementById("user-month");
+var userYear = document.getElementById("user-year");
+var submitButton = document.getElementById("submit-date");
+
+submitButton.addEventListener("click", function(evt){
+var message = document.getElementById('message');
+
+
+var parsedDay = parseInt(userDay.value);
+var parsedMonth = (parseInt(userMonth.value)-1);
+var parsedYear = parseInt(userYear.value);
+var months = ["January","February","March","April","May","June","July","August",
+"September","October","November","Dcember"];
+var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
+"Saturday"];
+var dateIs = new Date(parsedYear, parsedMonth, parsedDay);
+var weekdayIs = weekdays[dateIs.getDay()];
+var monthIs = months[parsedMonth];
+
+if(weekdayIs === undefined || monthIs === undefined) {
+  message.innerText = "";
+  setTimeout(function() {
+    message.innerText = "Please enter a valid date";}, 500)
+}
+else {
+  message.innerText = "Day:" + weekdayIs +"\n"+"Month:" + monthIs + "\n"+"Date:" + parsedDay +"\n"+"Year:" + parsedYear;
+}
+
+  })

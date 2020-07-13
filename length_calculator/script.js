3a3cfb43cd0dc51cc3be8b484466097152f9bada
Shashwath Mohan <55274230+shashwathmohan@users.noreply.github.com>
2020-07-13 14:42:19 +0530

    update

---
+++ length_calculator/script.js
@@ -0,0 +1,6 @@
+var print = function(msg) {
+  document.getElementById('output').innerHTML= "Length is :" +" "+ msg;
+}
+document.getElementById("btn").onclick=function(event){
+  print(document.getElementById('str').value.length);
+}
